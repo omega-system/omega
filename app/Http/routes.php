0741c25ee711da110a499c6c150d7d70882a80b3
lujjjh <i@codehut.me>
2016-04-13 10:40:27 +0800

    User authentication
    Add database migrations, views and controllers for user authentication.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,10 +7,17 @@
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| and give it the ioncontroller to call when that URI is requested.
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication
+$this->get('login', 'Auth\AuthController@showLoginForm');
+$this->post('login', 'Auth\AuthController@login');
+$this->get('logout', 'Auth\AuthController@logout');
+
+Route::get('/home', 'HomeController@index');
