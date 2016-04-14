a20b914d8a073e89fe41c089f596e75dad2da8bf
lujjjh <i@codehut.me>
2016-04-14 16:25:08 +0800

    修复 DB_HOST 配置, 加入数据库迁移

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,13 +30,25 @@ fi
 
 sed -i.bak "s/APP_ENV=local/APP_ENV=production/" .env
 sed -i.bak "s/APP_DEBUG=true/APP_DEBUG=false/" .env
-sed -i.bak "s/DB_HOST=127.0.0.1/DB_HOST=db/" .env
+sed -i.bak "s/DB_HOST=127.0.0.1/DB_HOST=${OMEGA_DB_HOST}/" .env
 sed -i.bak "s/DB_PORT=3306/DB_PORT=${OMEGA_DB_PORT}/" .env
 sed -i.bak "s/DB_DATABASE=homestead/DB_DATABASE=${OMEGA_DB_NAME}/" .env
 sed -i.bak "s/DB_USERNAME=homestead/DB_USERNAME=${OMEGA_DB_USER}/" .env
 sed -i.bak "s/DB_PASSWORD=secret/DB_PASSWORD=${OMEGA_DB_PASSWORD}/" .env
 
 # Create database if not exists
-php -r 'for ($i = 10; $i--; ) try { (new PDO("mysql:host=$argv[1];port=$argv[2]", $argv[3], $argv[4]))->exec("CREATE DATABASE IF NOT EXISTS \`$argv[5]\`"); exit(0); } catch (Exception $e) { printf("warning: " . $e); sleep(10); } exit(1);' -- $OMEGA_DB_HOST $OMEGA_DB_PORT $OMEGA_DB_USER $OMEGA_DB_PASSWORD $OMEGA_DB_NAME || exit 1
+php -r '
+for ($i = 10; $i--; ) {
+  try {
+    $pdo = new PDO("mysql:host=$argv[1];port=$argv[2]", $argv[3], $argv[4]);
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS `$argv[5]`"); exit(0);
+  } catch (Exception $e) {
+    printf("warning: " . $e);
+    sleep(10);
+  }
+}
+exit(1);' -- $OMEGA_DB_HOST $OMEGA_DB_PORT $OMEGA_DB_USER $OMEGA_DB_PASSWORD $OMEGA_DB_NAME || exit 1
+
+php artisan migrate --force
 
 exec "$@"
