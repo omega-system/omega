f55ace6804b5e98acec952750400f04e47acebca
lujjjh <i@codehut.me>
2016-04-15 11:34:35 +0800

    添加 name 列到 fillable

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     protected $fillable = [
-        'number', 'password',
+        'number', 'name', 'password',
     ];
 
     protected $hidden = [
