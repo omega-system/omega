82797734a8a5ed4bcdd72d4726f2462f215b7dc5
lujjjh <i@codehut.me>
2016-04-15 22:38:07 +0800

    主页

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the ioncontroller to call when that URI is requested.
-|
-*/
-
-// Secure test
-Route::get('secure', function () {
-    return ['secure' => Request::secure()];
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Authentication
-$this->get('login', 'Auth\AuthController@showLoginForm');
+$this->get('login', ['as' => 'login', 'uses' => 'Auth\AuthController@showLoginForm']);
 $this->post('login', 'Auth\AuthController@login');
-$this->get('logout', 'Auth\AuthController@logout');
+$this->get('logout', ['as' => 'logout', 'uses' => 'Auth\AuthController@logout']);
 
-Route::get('/home', 'HomeController@index');
+$this->get('/', ['as' => 'home', function () {
+    return view('welcome');
+}]);
