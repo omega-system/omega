823c8fc2bdfdd64fd5c163959e007cb332a9efe5
lujjjh <i@codehut.me>
2016-04-20 15:06:50 +0800

    创建 unique 索引，调整字段顺序

--- database/migrations/2016_04_20_063615_create_course_classes_table.php
+++ database/migrations/2016_04_20_063615_create_course_classes_table.php
@@ -16,13 +16,14 @@ class CreateCourseClassesTable extends Migration
             $table->increments('id')->unsigned()->index();
             $table->integer('trimester_id')->unsigned();
             $table->foreign('trimester_id')->references('id')->on('trimesters')->onDelete('cascade');
-            $table->integer('class_number')->unsigned();
             $table->integer('course_id')->unsigned();
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->integer('class_number')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('location');
             $table->timestamps();
+            $table->unique(['trimester_id', 'course_id', 'class_number']);
         });
     }
 
