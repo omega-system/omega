23ed357374c46a8e6c6c8e27dd9424c45b9542db
lujjjh <i@codehut.me>
2016-04-20 14:54:25 +0800

    创建课程班级表

---
+++ database/migrations/2016_04_20_063615_create_course_classes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCourseClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_classes', function (Blueprint $table) {
+            $table->increments('id')->unsigned()->index();
+            $table->integer('trimester_id')->unsigned();
+            $table->foreign('trimester_id')->references('id')->on('trimesters')->onDelete('cascade');
+            $table->integer('class_number')->unsigned();
+            $table->integer('course_id')->unsigned();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('course_classes');
+    }
+}
